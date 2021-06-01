124272501d768d8c62bc76a5a4c7fa07cb73d7ec
gouldcs <cam@gouldcs.com>
2021-05-31 23:03:28 -0700

    created project

---
+++ src/results.js
@@ -0,0 +1,29 @@
+var SerpWow = require('google-search-results-serpwow')
+let serpwow = new SerpWow('B8BE8C002F9E4921B003016FE8A894A4')
+
+let i = 1;
+for(let page = 1; page <= 4; page++) {
+  var params = {
+    q: '"should be concerned" OR "should be worried"  OR "are worried"',
+    search_type: 'news',
+    news_type: 'all',
+    time_period: 'last_day',
+    sort_by: 'relevance',
+    num: '100',
+    gl: 'us',
+    page: page.toString
+  }
+  
+  query = serpwow.json(params)
+    .then (queryOut => {
+      result = JSON.parse(JSON.stringify(queryOut, 0, 2))
+  
+      for (res of result.news_results) {
+        console.log(i + ". " + res.source + ": " + res.snippet)
+        i++
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    }); 
+}
